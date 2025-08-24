a1550f760293e807c808f3683d798dd5b63a628c
Your Name <your.email@example.com>
2025-08-24 16:35:23 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [react(), tailwindcss()],
 });
